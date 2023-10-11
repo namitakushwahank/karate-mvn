ce3be5088a2e502ac6383f46960eaf82701b2fde
namitakushwahank <namitakushwaha4@gmail.com>
2023-10-11 17:14:51 +0530

    Namita | adding reporting

--- src/test/java/testRunner.java
+++ src/test/java/testRunner.java
@@ -1,6 +1,14 @@
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
 import org.junit.jupiter.api.Test;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class testRunner {
 
@@ -9,6 +17,18 @@ public class testRunner {
         int threadCount = Integer.parseInt(System.getProperty("karate.threadCount", "5"));
         Results results = Runner
                 .path("src/test/java/features")
+                .outputJunitXml(true)
                 .parallel(threadCount);
+        generateReport(results.getReportDir());
+
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true); // Corrected this line
+        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "Karate API : Digital Ads");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
     }
 }
